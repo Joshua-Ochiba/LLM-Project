ce21b2f5cc2ce860453f4f8ba9e6ca603a5e228d
Joshua-Ochiba <157308207+Joshua-Ochiba@users.noreply.github.com>
2025-09-17 16:56:34 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 
-import Login from './components/Login/Login'
 import { ContentPage } from './pages/ContentPage'
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/ContentPage' element={<ContentPage/>}/>
-      </Routes>
-      </BrowserRouter>
+    <ContentPage/>
   )
 }
 
